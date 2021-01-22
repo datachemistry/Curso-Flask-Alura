f83152376cd574e1c2a3bf43719e18bc476e0ad7
datachemistry <biorrefinaria@gmail.com>
2021-01-22 10:02:24 -0300

    Update 22-01-21

--- Curso Pt 2/prepara_banco.py
+++ Curso Pt 2/prepara_banco.py
@@ -6,22 +6,22 @@ conn = MySQLdb.connect(user='root', passwd='admin', host='localhost', port=3306)
 #conn.cursor().execute("DROP DATABASE `jogoteca`;")
 #conn.commit()
 
-criar_tabelas = '''SET NAMES utf8;
-    CREATE DATABASE `jogoteca` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-    USE `jogoteca`;
-    CREATE TABLE `jogo` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `nome` varchar(50) COLLATE utf8_bin NOT NULL,
-      `categoria` varchar(40) COLLATE utf8_bin NOT NULL,
-      `console` varchar(20) NOT NULL,
-      PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-    CREATE TABLE `usuario` (
-      `id` varchar(8) COLLATE utf8_bin NOT NULL,
-      `nome` varchar(20) COLLATE utf8_bin NOT NULL,
-      `senha` varchar(8) COLLATE utf8_bin NOT NULL,
-      PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;'''
+#criar_tabelas = '''SET NAMES utf8;
+#    CREATE DATABASE `jogoteca` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+#    USE `jogoteca`;
+#    CREATE TABLE `jogo` (
+#      `id` int(11) NOT NULL AUTO_INCREMENT,
+#      `nome` varchar(50) COLLATE utf8_bin NOT NULL,
+#      `categoria` varchar(40) COLLATE utf8_bin NOT NULL,
+#      `console` varchar(20) NOT NULL,
+#      PRIMARY KEY (`id`)
+#    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+#    CREATE TABLE `usuario` (
+#      `id` varchar(8) COLLATE utf8_bin NOT NULL,
+#      `nome` varchar(20) COLLATE utf8_bin NOT NULL,
+#      `senha` varchar(8) COLLATE utf8_bin NOT NULL,
+#      PRIMARY KEY (`id`)
+#    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;'''
 
 conn.cursor().execute(criar_tabelas)
 
